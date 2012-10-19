8420a6249eac8ce9348614c4aae19ed30134f8b0
hs3ldon <pvm@fastmail.fm>
2012-10-19 12:17:12 +0200

    Initial site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kairos::Application.config.secret_token = '26f2a5aea77df3ee862193f74e8bdf88b9b1af1dc02d26a6cb3929173f762a28c81772b4f06355038f2a06f6686d551a866c9baf4ab01a74edacdac8bae2fcc7'
